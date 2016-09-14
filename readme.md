a421dd5ff087452fc7a56fe04b268e0383cef6f8
maxshine <max8hine@gmail.com>
2016-09-14 12:32:18 +1200

    for 1.0
    for 1.0

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Max Ma Site
+
+
+** I dont have time to write this :) **
+
+This has a Gulp-powered build system with these features:
+
+- Handlebars HTML templates with Panini
+- Sass compilation and prefixing
+- JavaScript concatenation
+- Built-in BrowserSync server
+- For production builds:
+  - CSS compression
+  - JavaScript compression
+  - Image compression
---
+++ src/assets/js/app.js
@@ -0,0 +1,20 @@
+$(document).foundation();
+
+$('#homeSlider').flexslider({
+  animation: "slide",
+  controlNav: false,
+  directionNav:false,
+  slideshowSpeed: 4000
+});
+
+$('#websiteSlider').flexslider({
+  animation: "slide",
+  controlNav: false,
+  prevText: "",    
+  nextText: ""
+});
+
+$('#poster').flexslider({
+  animation: "slide",
+  controlNav: false
+});
